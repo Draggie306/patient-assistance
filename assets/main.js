e423292b3c458491ea41680e8e313f8c28224fb4
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2024-07-04 19:55:30 +0100

    (hour 2) (incomplete) Add partial implementation of websocket URLs

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,45 @@
 console.log("Hello, mum!");
 
+// Button element IDs are: mainHelpButton, hugButton, stairsButton, waterButton, altButton
+// txtBox contains the content of the custom message to be sent
+// servURL contains the websocket URL and is hidden once the connection is established
+
+// Main button! The one right in the centre of the screem
 var mainButton = document.getElementById("mainHelpButton");
 
 mainButton.addEventListener("click", function() {
-    console.log(`Hello again at ${Date.now()}`);
+    console.log(`The main button has been clicked at ${new Date()}`);
+    sendSocketMessage("mainButton");
 })
+
+
+
+// Cjeck previous input for websocket URL
+
+function getPreviouslyConnectedWsURL() {
+    // if localstorage isConnected is true AND localstorage wsURL is string
+    if (localStorage.getItem("isConnected") === "true" && typeof localStorage.getItem("wsURL") === "string") {
+        // then set isPreviouslyConnected to true
+        isPreviouslyConnected = true;
+        var msgBoxForWsURL = document.getElementById("wsURL");
+        msgBoxForWsURL.style.display = "none";
+        return localStorage.getItem("wsURL");
+    } else {
+        return "ws://localhost:8080";
+    }
+}
+
+function handleChangedWsURL(type) {
+    if (type === 1) {
+        var msgBoxForWsURL = document.getElementById("wsURL");
+        var newWsURL = msgBoxForWsURL.value;
+        localStorage.setItem("wsURL", newWsURL);
+        msgBoxForWsURL.style.display = "none";
+        isPreviouslyConnected = true;
+        return newWsURL;
+    } else {
+        // clear
+        localStorage.removeItem("wsURL");
+        localStorage.removeItem("isConnected");
+    }
+}
--- assets/socketry.js
+++ assets/socketry.js
@@ -1,12 +1,29 @@
 console.log("Socketry (not sorcery) script loaded.");
 
+var socketStatus = null;
+
 try {
     // Create WebSocket connection.
-    const socket = new WebSocket("ws://localhost:8080");
+    console.log("Creating WebSocket connection...");
 
-    // Connection opened
+    var wsUrl = getPreviouslyConnectedWsURL();
+
+    const socket = new WebSocket(wsUrl);
+    
+    // error if the connection is not established
+    socket.addEventListener("error", (event) => {
+        console.error("Error in socket connection.");
+        notifyMe("Error in socket connection.");
+        socketStatus = 0;
+        throw new Error("Error in socket connection.");
+    });
+
+    // ONLY IF the connection is established:
     socket.addEventListener("open", (event) => {
+        console.log("Connection opened successfully");
+        socketStatus = 1;
         socket.send("Hello Server!");
+        console.log("Message sent to server.");
     });
 
     socket.addEventListener("message", (event) => {
@@ -14,5 +31,18 @@ try {
     });
 } catch (error) {
     console.error(`Error in socketry script: error ${error}`);
-    notifyMe(`Error in socketry script was ${error}`);
+    window.alert(`Error in socketry script was ${error}`);
+    var socketStatus = 0;
+}
+
+function sendSocketMessage(message) {
+    if (socketStatus === 1) {
+        socket.send(message);
+    } if (socketStatus === 0) {
+        console.error(`The socket failed to connect to ${wsUrl}`);
+        window.alert(`The socket failed to connect to ${wsUrl}`);
+    } else {
+        console.error("The socket is being created, please wait a moment.");
+        window.alert("The socket is being created, please wait a moment.");
+    }
 }
