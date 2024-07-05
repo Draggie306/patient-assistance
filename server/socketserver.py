b15c830249f9d2d311bd28e0e10f547e64ecfa56
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2024-07-05 23:50:31 +0100

    (hour 6) update server WebSocket handler
    - Added OOP classes for each patient and assister
    - Handler rewritten to better parse unique events
    - Better debugging statements
    - and some more!

--- server/socketserver.py
+++ server/socketserver.py
@@ -9,21 +9,72 @@ patients = {}
 assisters = {}
 
 
+# OOP class for patients and assisters, added to a dictionary of the respective type
+# Patients and assisters are identified by their IDs. Assisters are assigned to patients by their IDs.
+# PatientIDs are determinedclient-side through the current datetime in js, so should be unique.
+# Also, the websocket is the completely unique identifier for each patient.
+
+class Patient:
+    def __init__(self, websocket, patientID, userAgent, joinedAssister=[]):
+        self.websocket = websocket  # this is the unique websocket object for each patient
+        self.patientID = patientID
+        self.userAgent = userAgent
+        self.joinedAssister = joinedAssister
+
+    async def send(self, message: str) -> None:
+        await self.websocket.send(message)
+
+    async def addJoinedAssister(self, websocket) -> None:
+        self.joinedAssister.append(websocket)
+
+    async def messageAllAssisters(self, message: str) -> None:
+        for assister in assisters:
+            await assister.websocket.send(json.dumps({"type": "patientMessage", "message": message}))
+
+
+class Assister:
+    def __init__(self, assisterID, websocket):
+        self.assisterID = assisterID
+        self.websocket = websocket
+
+
 async def handler(websocket) -> None:
     while True:
         message = await websocket.recv()
 
-        if message == "Hello, server!":
+        # Decode the json into patientID, userAgent, and "message" (the actual message sent by the client)
+        message = json.loads(message)
+
+        patientID = message["patientID"] if "patientID" in message else None
+        clientUserAgent = message["userAgent"]
+        actualMessage = message["message"]
+
+
+        # Handshake message from a patient
+        if actualMessage == "Hello, server!":
             print("Matched handshake message from a patient")
             await websocket.send("success")
             await websocket.send(f"Current time: {datetime.datetime.now()}")
-            patients[websocket] = time.time()
+            patients[patientID] = Patient(websocket, patientID, clientUserAgent)
 
-        if message == "assister":
+        if actualMessage == "mainButton":
+            print("Matched main button press")
+            await relay_message_to_client("mainButton", "main button pressed")
+
+        # Now handshake message from an assister
+        if actualMessage == "assister":
             print("Matched handshake message from an assister")
             await websocket.send("success")
             await websocket.send(f"Current time: {datetime.datetime.now()}")
-            assisters[websocket] = time.time()
+
+        # Display list of all patients on the current list to the assister frontend
+        if actualMessage == "getAllPatients":
+            print("Matched request for all patients")
+            await websocket.send(json.dumps(patients))
+
+        if actualMessage == "registerAssister":
+            print(f"Matched request to register an assister to patient {patientID}")
+            patients[patientID].addJoinedAssister(websocket)
 
         print(message)
 
@@ -64,6 +115,7 @@ async def relay_message_to_client(msg_type: str, message: typing.Optional[str]) 
 
 
 if __name__ == "__main__":
+    print("running!")
     asyncio.run(main())
 
 
