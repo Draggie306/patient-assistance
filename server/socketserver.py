437a66d5d07436af1b49168dd629369b6da57add
Draggie306 <75524491+Draggie306@users.noreply.github.com>
2024-07-07 22:13:29 +0100

    Partially implement new functionality and improved forwarding responses

--- server/socketserver.py
+++ server/socketserver.py
@@ -38,12 +38,27 @@ class Assister:
         self.websocket = websocket
 
 
+def build_json_response(shorthand: str, message: str) -> str:
+    """
+    Builds a JSON response to send to the client.
+    `shorthand` is a string that the JS uses to determine how best to handle the message.
+    `message` is the actual message to send to the client.
+    """
+    response = {
+        "shorthand": shorthand,
+        "message": message
+    }
+    return json.dumps(response)
+
+
 async def handler(websocket) -> None:
     while True:
         message = await websocket.recv()
+        print(message)
 
         # Decode the json into patientID, userAgent, and "message" (the actual message sent by the client)
         message = json.loads(message)
+        print(message)
 
         patientID = message["patientID"] if "patientID" in message else None
         clientUserAgent = message["userAgent"]
@@ -53,13 +68,18 @@ async def handler(websocket) -> None:
         # Handshake message from a patient
         if actualMessage == "Hello, server!":
             print("Matched handshake message from a patient")
-            await websocket.send("success")
-            await websocket.send(f"Current time: {datetime.datetime.now()}")
-            patients[patientID] = Patient(websocket, patientID, clientUserAgent)
+            # await websocket.send("success")
+            await websocket.send(build_json_response('SUCCESS', 'handshakeAck'))
+            registerPatient = await register_patient(websocket, patientID, clientUserAgent)
+
+            if registerPatient:
+                await websocket.send("registerSuccess")
+            else:
+                await websocket.send("registerErr")
 
         if actualMessage == "mainButton":
             print("Matched main button press")
-            await relay_message_to_client("mainButton", "main button pressed")
+            await relay_message_to_assister("mainButton", "Main button pressed by patient")
 
         # Now handshake message from an assister
         if actualMessage == "assister":
@@ -70,13 +90,63 @@ async def handler(websocket) -> None:
         # Display list of all patients on the current list to the assister frontend
         if actualMessage == "getAllPatients":
             print("Matched request for all patients")
-            await websocket.send(json.dumps(patients))
+            # await websocket.send(json.dumps(patients))
+            currentPatients = await query_patients()
+            await websocket.send(json.dumps(currentPatients))
 
         if actualMessage == "registerAssister":
             print(f"Matched request to register an assister to patient {patientID}")
             patients[patientID].addJoinedAssister(websocket)
 
-        print(message)
+        
+
+
+async def send_message(Patient, message: str) -> None:
+    """
+    Sends a message from a patient to each registered assister.
+    `Patient` is the patient object that sent the message.
+    `message` - the message from the patient to the assisters.
+    """
+
+    # Each message should be a JSON response returned with essential keys: "shorthand" and "message"
+    # "shorthand" is a string that the JS uses to determine how best to handle themessage
+    # current shorthands are always prefixed by "assistanceapp." and can be one of:
+    # "SUCCESS", "NO_ASSISTERS", "ERROR_FORWARDING", "ERROR_PARSING", "SERVER_EXCEPTION"
+    # when using "SUCCESS" another key "numAssisters" is required
+
+    # simple forwarder:
+
+
+
+
+    pass
+
+
+async def register_patient(websocket, patientID, userAgent) -> None:
+    """
+    Registers a patient with the server.
+    """
+    try:
+        patients[patientID] = Patient(websocket, patientID, userAgent)
+        print(f"Registered patient {patientID}")
+        return True
+    except Exception as e:
+        print(f"Error registering patient {patientID}: {e}")
+        return False
+
+
+async def query_patients() -> None:
+    """
+    Incoming call from assister view page to display all patients in the current list, their IDs, UserAgents, and total number of assisters.
+    """
+    print("Querying all patients")
+    patientsList = []
+    for patient in patients:
+        patientsList.append({"patientID": patient.patientID, "userAgent": patient.userAgent, "numAssisters": len(patient.joinedAssister)})
+
+    print(patientsList)
+    return patientsList
+
 
 
 async def main():
@@ -84,15 +154,14 @@ async def main():
         await asyncio.Future()  # run forever
 
 
-async def relay_message_to_client(msg_type: str, message: typing.Optional[str]) -> None:
+async def relay_message_to_assister(shorthand: str, message: typing.Optional[str]) -> None:
     """
     Transforms and sends a message received by one patient to all registered assisters.
     """
     print("Relaying message to all assisters")
 
     for patient in patients:
-        await patient.send(json.dumps({"type": msg_type, "message": message})) # do minimal server processing, this is decoded by the client
-        print(f"Relayed message to patient {patient}; sent {message}")
+        await patient.messageAllAssisters(json.dumps({"shorthand": shorthand, "message": message}))
 
 
 
